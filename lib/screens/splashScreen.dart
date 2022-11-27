9a6c879dda85eeb3ec258e90ba72f33ffac9528a
Jordano Iqbal Darmawan <79011617+jordanoid@users.noreply.github.com>
2022-11-28 00:07:15 +0700

    splashscreen done

--- lib/screens/splashScreen.dart
+++ lib/screens/splashScreen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:game_info_app/main.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreenPage extends StatefulWidget {
   const SplashScreenPage({Key? key}) : super(key: key);
@@ -29,16 +30,24 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black87,
-      body: Container(
-        alignment: Alignment.bottomCenter,
-        child: Image.asset(
-          "assets/images/splash2.gif",
-          width: 250,
-          height: 250,
-          gaplessPlayback: true,
-        ),
-      ),
-    );
+        backgroundColor: Colors.black87,
+        body: Container(
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 250,
+                ),
+                Image.asset(
+                  "assets/images/splash2.gif",
+                  width: 250,
+                  height: 250,
+                  gaplessPlayback: true,
+                ),
+                Text("Game Information",
+                    style: GoogleFonts.contrailOne(
+                        fontSize: 30, color: Colors.white))
+              ],
+            )));
   }
 }
